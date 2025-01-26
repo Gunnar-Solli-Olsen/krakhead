012f9207ba6f1c524eaeafaabb8fd4e2da8629e7
Gunnar-Solli-Olsen <guols6589@uit.no>
2025-01-26 01:09:00 +0100

    add sfx :)

--- player.gd
+++ player.gd
@@ -8,8 +8,10 @@ signal on_platform
 @export var SWIM_FORCE = 400
 @export var SWIM_GRAVITY = 100
 @export var SWIM_SPEED = 150
+
 const SUCTION_FORCE = 1000  # Stronger suction force to drag the player down
 const SWIM_LIMIT = -500  # Make swimming force weaker when inside suction zone
+@onready var _animated_sprite = $Sprite
 
 var min_x = 0
 var max_x = 715
@@ -18,6 +20,7 @@ var max_y = 420
 # Variables
 var is_in_water = false
 var is_in_suction_cone = false
+var is_on_platform = false
 # switch to play water enter animation, 
 # sounds and slow player falling speed down
 var entered_water = false 
@@ -37,14 +40,16 @@ func _physics_process(delta):
 	var direction = Vector2.ZERO
 	if Input.is_action_pressed("ui_left"):
 		direction.x -= 1
-		$Sprite2D.flip_h = true
+		_animated_sprite.flip_h = true
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
-		$Sprite2D.flip_h = false
+		_animated_sprite.flip_h = false
+		
 
 	# If player enters water turn on water physics
 	if global_position.y > 150:
 		is_in_water = true
+
 		if (not entered_water): # entering water should lower speed 
 			velocity.y*=4/6
 			entered_water = true
@@ -57,10 +62,16 @@ func _physics_process(delta):
 		if is_in_suction_cone:
 			# Apply suction force directly overriding swimming
 			velocity.y += SUCTION_FORCE * delta  # Strong suction effect, no swimming allowed
+			if (not $MakingBubble.playing):
+				$MakingBubble.play(0)
 		elif Input.is_action_pressed("ui_up"):
+			if ($MakingBubble.playing):
+				$MakingBubble.stop()
 			velocity.y += (SWIM_GRAVITY - SWIM_FORCE) * delta
 			#print("swimforce: ", (SWIM_GRAVITY - SWIM_FORCE) * delta)
 		else:
+			$MakingBubble.stop()
+
 			velocity.y += SWIM_GRAVITY * delta
 			#print("fallforce: ", (SWIM_GRAVITY) * delta)
 				# Apply horizontal speed
@@ -77,9 +88,22 @@ func _physics_process(delta):
 	# Limit fall speed
 	velocity.y = clamp(velocity.y, -MAX_FALL_SPEED, MAX_FALL_SPEED)
 	
-	if (Input.is_action_pressed("SOS") and abs(velocity.y) < 1):
-		print("sos")
+	if (is_on_platform): # change this to be difference between platform and player 
+		$SwimmingPlayer.stop()
+		if (direction.x == 0):
+			_animated_sprite.play("idle")
+			$WalkingOnPlatform.stop()	
+
+		else:
+			_animated_sprite.play("walk")
+			if (not $WalkingOnPlatform.playing):
+				$WalkingOnPlatform.play(0)
+
+		if (Input.is_action_pressed("SOS")):
+			print("sos")
 		# Get the current position of the CharacterBody3D node
+	else:
+		_animated_sprite.play("swim")
 	var current_position = global_position
 	
 	# Clamp the x position
@@ -97,6 +121,8 @@ func _physics_process(delta):
 # Update the water state when the player enters water
 func _on_water_body_entered(body: Node2D) -> void:
 	print("player entered water")
+	$SwimmingPlayer.play(0.45)
+	$splash.play(0)
 	pass
 	#if body.name == "Player":
 	#	is_in_water = true
@@ -112,11 +138,15 @@ func _on_water_body_exited(body: Node2D) -> void:
 func _on_platform_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
 		print("player on platform")
+		is_on_platform = true
 		#emit_signal("player_entered", self)
 
 func _on_platform_body_exited(body: Node2D) -> void:
 	if body.name == "Player":
 		print("player left platform")
+		is_on_platform = false
+		$WalkingOnPlatform.stop()
+
 		#emit_signal("player_exited", self)
 
 # Called when the player enters the suction cone
